006bd877da5eb73c43849466c953957850d619ee
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2023-03-08 02:57:09 +0000

    updated DataItem comp & tests

--- components/__tests__/dataItem.test.tsx
+++ components/__tests__/dataItem.test.tsx
@@ -1,15 +1,90 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import DataItem from "../dataItem";
 
 describe('DataItem Component', () => {
     let container: HTMLElement;
 
+    const props = {
+        seriesTitle: "Visualization Series Title",
+        data: "Series Data received by props",
+        dataActions: [
+            {
+                testId: "move-action",
+                text: "Move",
+                btnType: "button",
+                clickHandle: jest.fn(),
+
+            },
+            {
+                testId: "view-action",
+                text: "View",
+                btnType: "button",
+                clickHandle: jest.fn(),
+
+
+            },
+            {
+                testId: "edit-action",
+                text: "Edit",
+                btnType: "button",
+                clickHandle: jest.fn(),
+
+
+            },
+            {
+                testId: "delete-action",
+                text: "Delete",
+                btnType: "button",
+                clickHandle: jest.fn(),
+
+
+            },
+            {
+                testId: "download-action",
+                text: "Download",
+                btnType: "button",
+                clickHandle: jest.fn(),
+
+            }]
+    }
+
     beforeEach(() => {
-        const { container: renderContainer } = render(<DataItem />);
+        const { container: renderContainer } = render(<DataItem {...props} />);
         container = renderContainer;
     });
 
     it('should render without crash', () => {
         expect(container);
     });
+
+    it('should contain a DIV element with testId "data-item"', () => {
+        const divElement = screen.getByTestId("data-item");
+
+        expect(divElement.tagName).toBe("DIV");
+    });
+
+    it('should contain a H2 element with series title received by props', () => {
+        const headingElement = screen.getByTestId("series-title");
+
+        expect(headingElement.tagName).toBe("H2");
+        expect(headingElement).toHaveTextContent(props.seriesTitle);
+
+    });
+
+    it('should render the DataDisplay component with data received by props', () => {
+        const displayComponent = screen.getByTestId("data-display");
+
+        expect(displayComponent).toBeInTheDocument();
+        expect(displayComponent).toHaveTextContent(props.data);
+    });
+
+    it('should render the ActionButons component with data received by props', () => {
+        const actionsComponent = screen.getByTestId("action-buttons");
+
+        expect(actionsComponent).toBeInTheDocument();
+    });
+
+
+
+
 });
--- components/dataItem.tsx
+++ components/dataItem.tsx
@@ -1,5 +1,16 @@
+import ActionButtons from "./actionButtons";
+import DataDisplay from "./dataDisplay";
+
 function DataItem({ ...props }) {
-    return (<></>);
+
+    return (
+        <div data-testid="data-item">
+            <h2 data-testid="series-title">{props.seriesTitle}</h2>
+            <DataDisplay data={props.data} />
+            <ActionButtons dataActions={props.dataActions} />
+
+        </div>
+    );
 }
 
 export default DataItem;
