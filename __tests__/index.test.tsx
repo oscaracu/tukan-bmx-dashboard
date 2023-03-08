6b75d5d18b27eb1eb5ef03fb86f678fe5e3a17f2
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2023-03-08 19:04:32 +0000

    updated Home Comp & test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,15 +2,44 @@ import { render, screen } from '@testing-library/react'
 import Home from '@/pages/index'
 
 describe('Home Component', () => {
+
   let container: HTMLElement;
 
   beforeEach(() => {
+
     const { container: renderContainer } = render(<Home />);
     container = renderContainer;
+
   });
 
   it('renders without crash', () => {
+
     expect(container)
+
+  });
+
+  it('should contain the Header Component', () => {
+
+    const headerElement = screen.getByTestId("app-header");
+
+    expect(headerElement).toBeInTheDocument();
+
+  });
+
+  it('should contain the DataContainer Component', () => {
+
+    const dataContainer = screen.getByTestId("data-container");
+
+    expect(dataContainer).toBeInTheDocument()
+
+  });
+
+  it('should contain the Modal Component', () => {
+
+    const modalContainer = screen.getByTestId("modal");
+
+    expect(modalContainer).toBeInTheDocument();
+
   });
 
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,66 @@
-import SystemDescription from "@/components/systemDescription";
-
-const data = {
-  description: "Possimus aliquid aut consequuntur fuga magni qui eum est aut. Temporibus hic nihil nesciunt nam eaque. Dolorem officia repellat. Animi neque laboriosam."
-}
+import DataCointainer from "@/components/dataContainer";
+import Header from "@/components/header";
+import Modal from "@/components/modal";
 
 export default function Home() {
-  return <div>
 
+  const dataItems = [
+    {
+      seriesTitle: "Visualization Series Title 1",
+      data: "Series Data received by props 1",
+    },
+    {
+      seriesTitle: "Visualization Series Title 2",
+      data: "Series Data received by props 2",
+    },
+    {
+      seriesTitle: "Visualization Series Title 3",
+      data: "Series Data received by props 3",
+    },
+
+  ];
+
+  const dataActions = [
+    {
+      testId: "move-action",
+      text: "Move",
+      btnType: "button",
+      clickHandle: () => console.log("Move")
+
+    },
+    {
+      testId: "view-action",
+      text: "View",
+      btnType: "button",
+      clickHandle: () => console.log("View")
+
+
+    },
+    {
+      testId: "edit-action",
+      text: "Edit",
+      btnType: "button",
+      clickHandle: () => console.log("Edit")
+
+    },
+    {
+      testId: "delete-action",
+      text: "Delete",
+      btnType: "button",
+      clickHandle: () => console.log("Delete")
+
+    },
+    {
+      testId: "download-action",
+      text: "Download",
+      btnType: "button",
+      clickHandle: () => console.log("Download")
+    }]
+
+  return <div>
+    <Header />
+    <DataCointainer dataItems={dataItems} dataActions={dataActions} />
+    <Modal />
 
   </div>
 }
