f5874856380d78af0ebfdccd57ebf746f95d9068
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2023-03-12 06:40:48 +0000

    added hover btns styles

--- components/header.tsx
+++ components/header.tsx
@@ -1,33 +1,27 @@
 import Button from "./button";
 import { BsDownload, BsGrid, BsListUl } from "react-icons/bs";
 
-const headerBtnsStyles = "text-sky-900 text-3xl";
+const headerBtnsStyles = "text-sky-900 text-3xl p-1 hover:bg-sky-800 hover:text-white rounded-md";
 const paragraphStyle = "hidden";
 
 function Header({ ...props }) {
     return (
-        <header data-testid="app-header" className="flex flex-row gap-2 items-center justify-between">
+        <header data-testid="app-header" className="flex flex-row gap-2 items-center justify-between mb-10">
 
             <h1 data-testid="app-title" className="text-2xl">Visualizations</h1>
             <div className=" flex flex-row gap-3 items-center">
 
                 <Button btnType="button" testId="download-btn" clickHandle={props.clickHandle}>
+                    <div className={headerBtnsStyles}><BsDownload /></div>
                     <p className={paragraphStyle}>Download all</p>
-                    <span className={headerBtnsStyles}>
-                        <BsDownload />
-                    </span>
                 </Button>
                 <Button btnType="button" testId="grid-btn" clickHandle={props.clickHandle}>
+                    <div className={headerBtnsStyles}><BsGrid /></div>
                     <p className={paragraphStyle}>Grid view</p>
-                    <span className={headerBtnsStyles}>
-                        <BsGrid />
-                    </span>
                 </Button>
                 <Button btnType="button" testId="list-btn" clickHandle={props.clickHandle}>
+                    <div className={headerBtnsStyles}><BsListUl /></div>
                     <p className={paragraphStyle}>List view</p>
-                    <span className={headerBtnsStyles}>
-                        <BsListUl />
-                    </span>
                 </Button>
             </div>
 
