5f2adba005c22299e6fd3260a9fab323c0dac1b2
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2023-03-12 05:04:31 +0000

    added header mobile styles

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,35 @@
 import Button from "./button";
+import { BsDownload, BsGrid, BsListUl } from "react-icons/bs";
+
+const headerBtnsStyles = "text-sky-900 text-3xl";
+const paragraphStyle = "hidden";
 
 function Header({ ...props }) {
     return (
-        <header data-testid="app-header">
+        <header data-testid="app-header" className="flex flex-row gap-2 items-center justify-between">
 
-            <h1 data-testid="app-title">Visualizations</h1>
-            <Button btnType="button" testId="download-btn" clickHandle={props.clickHandle}>
-                <p>Download all</p>
-            </Button>
-            <Button btnType="button" testId="grid-btn" clickHandle={props.clickHandle}>
-                <p>Grid view</p>
-            </Button>
-            <Button btnType="button" testId="list-btn" clickHandle={props.clickHandle}>
-                <p>List view</p>
-            </Button>
+            <h1 data-testid="app-title" className="text-2xl">Visualizations</h1>
+            <div className=" flex flex-row gap-3 items-center">
+
+                <Button btnType="button" testId="download-btn" clickHandle={props.clickHandle}>
+                    <p className={paragraphStyle}>Download all</p>
+                    <span className={headerBtnsStyles}>
+                        <BsDownload />
+                    </span>
+                </Button>
+                <Button btnType="button" testId="grid-btn" clickHandle={props.clickHandle}>
+                    <p className={paragraphStyle}>Grid view</p>
+                    <span className={headerBtnsStyles}>
+                        <BsGrid />
+                    </span>
+                </Button>
+                <Button btnType="button" testId="list-btn" clickHandle={props.clickHandle}>
+                    <p className={paragraphStyle}>List view</p>
+                    <span className={headerBtnsStyles}>
+                        <BsListUl />
+                    </span>
+                </Button>
+            </div>
 
         </header>);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -140,7 +140,7 @@ export default function Home({ ...props }) {
   };
 
   return (
-    <div>
+    <div className="m-3">
       <Header />
       <DataCointainer
         dataItems={dataItems}
