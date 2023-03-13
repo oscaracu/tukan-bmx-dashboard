cf7940bf9659e845ad252db93b0b11a321589c71
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2023-03-13 03:30:49 +0000

    added table visualization type data display

--- components/dataDisplay.tsx
+++ components/dataDisplay.tsx
@@ -1,7 +1,34 @@
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
+
 function DataDisplay({ ...props }) {
+
+    let displayData;
+
+    if (props.data.visualizationType === "table") {
+        displayData =
+            <div className=" w-full max-w-xs overflow-scroll max-h-40">
+                <table className="table-auto w-full bg-white text-center border-2">
+                    <tr>
+                        <th className="border-2 border-slate-400 border-solid bg-sky-100">Fecha</th>
+                        <th className="border-2 border-slate-400 border-solid">Dato</th>
+                    </tr>
+                    {props.data.dataQuery.datos.map((dato: { fecha: string; dato: string; }, index: number) =>
+                        <tr>
+                            <td className="border-2 border-slate-400 border-solid bg-sky-100">{dato.fecha}</td>
+                            <td className="border-2 border-slate-400 border-solid">{dato.dato}</td>
+                        </tr>
+                    )}
+                </table>
+            </div>;
+
+    } else {
+        console.log("graph");
+
+    }
+
     return (
         <div data-testid="data-display">
-            <p className="hidden">{JSON.stringify(props.data)}</p>
+            {displayData}
         </div>);
 }
 
--- components/modal.tsx
+++ components/modal.tsx
@@ -1,6 +1,6 @@
 function Modal({ ...props }) {
     return (
-        <div data-testid="modal" className="absolute top-0 left-0 w-full h-auto min-h-screen bg-slate-800/95 flex flex-col items-center justify-center p-3 sm:h-auto">
+        <div data-testid="modal" className="top-0 left-0 w-full h-auto min-h-screen fixed bg-slate-800/95 flex flex-col items-center justify-center p-3 sm:h-auto">
             {props.childComponent}
         </div>);
 }
